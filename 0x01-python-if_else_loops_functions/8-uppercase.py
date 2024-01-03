5b93968295e83903eaeba9540c5d3ff9c21282f0
ManalBen1 <manatini@gmail.com>
2024-01-03 07:42:01 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
 def uppercase(s):
+    result = ""
     for char in s:
         ascii_value = ord(char)
         if 97 <= ascii_value <= 122:
             ascii_value -= 32
-        print("{:c}".format(ascii_value), end="")
-    print()  # Print new line
+        result += "{:c}".format(ascii_value)
+    print(result)  # Print the modified string
 
 # Test cases
 uppercase("best")
 uppercase("Best School 98 Battery street")
 uppercase("Holberton")
+uppercase("Holberton School")
+uppercase("Holberton School, 98 battery street")
+uppercase("")
+uppercase(98)
+uppercase("z")
