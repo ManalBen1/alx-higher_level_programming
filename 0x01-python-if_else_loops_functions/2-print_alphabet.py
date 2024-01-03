edec8618a37ed30c5639f6c42964c16d919e2d49
ManalBen1 <manatini@gmail.com>
2024-01-03 07:12:11 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    print(chr(letter), end="")
-print(chr(ord('z')))
+
+for letter in range(ord('a'), ord('z')+1):
+    print("{}".format(chr(letter)), end="" if letter != ord('z') else "\n")
