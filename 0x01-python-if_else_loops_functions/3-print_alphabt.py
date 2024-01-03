8005dc75613a37f6cab76029cd05aba4b751ba56
ManalBen1 <manatini@gmail.com>
2024-01-03 07:32:26 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,6 @@
 
 for letter in range(ord('a'), ord('z') + 1):
     if chr(letter) not in ['q', 'e']:
-        print("{:s}".format(chr(letter), end=""))
+        print("{:s}".format(chr(letter)), end="")
+
+# No newline character is added to match the expected output
