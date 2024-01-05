81be06e7b4e0930745e14e5f4717db96a3685b96
ManalBen1 <manatini@gmail.com>
2024-01-04 18:09:22 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+
+# Define variables a and b
 a = 1
 b = 2
-exec(compile(open("add_0.py").read(), "add_0.py", 'exec'))
+
+# Execute the contents of add_0.py using exec and compile
+exec(compile(open("add_0.py").read(), "add_0.py", 'exec'), globals())
+
+# Call the add function and store the result
 result = add(a, b)
+
+# Print the result using string formatting
 print("{} + {} = {}".format(a, b, result))
