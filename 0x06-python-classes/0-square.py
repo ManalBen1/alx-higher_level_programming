ee57a93c5d160a690a4fda5da37a7cbddc43b6bb
ManalBen1 <manatini@gmail.com>
2024-02-01 14:21:08 -0800

    files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
-class Square:
-    pass
 
+"""
+Module Description: This module defines a Square class representing a square shape.
+"""
+
+class Square:
+    """
+    Square class represents a square with certain properties.
+
+    Attributes:
+    - size (int): Represents the size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+        - size (int): Size of the square (default is 0).
+        """
+        self.size = size
